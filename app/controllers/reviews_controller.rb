d9832cfd2a3459ecb14be85e29afb87790bd86cd
Brian Firestone <bfirestone2339@gmail.com>
2021-02-03 18:26:15 -0500

    Created /reviews and /reviews/new actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,10 @@
 class ReviewsController < ApplicationController
 
+    get '/reviews' do
+        erb :'reviews/reviews'
+    end
+
+    get '/reviews/new' do
+        erb :'reviews/new'
+    end
 end
