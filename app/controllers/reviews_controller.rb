229ce7de8b9700c493d1a7d954b96a0a0bef28f2
Brian Firestone <bfirestone2339@gmail.com>
2021-02-04 13:56:14 -0500

    Created reviews DELETE action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,6 +17,11 @@ class ReviewsController < ApplicationController
         erb :'reviews/show'
     end
 
+    get '/reviews/:id/edit' do
+        @review = Review.find_by_id(params[:id])
+        erb :'reviews/edit'
+    end
+
     post '/reviews' do
         @user = User.find_by_id(session[:user_id])
         if params[:existing_movie]
@@ -33,5 +38,19 @@ class ReviewsController < ApplicationController
             @review.save
         end
         redirect to "/reviews/#{@review.id}"
+    end
+
+    delete '/reviews/:id' do 
+        if logged_in?
+            @review = Review.find_by_id(params[:id])
+            if @review && @review.user == current_user
+                @review.destroy
+                redirect to "/welcome/#{@review.user.slug}"
+            else
+                redirect to "/welcome/#{@review.user.slug}"
+            end
+        else
+            redirect to "/index"
+        end
     end
 end
