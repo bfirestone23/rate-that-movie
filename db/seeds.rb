7cb02ea02c553d6e350a582878eb444aa384130c
Brian Firestone <bfirestone2339@gmail.com>
2021-02-03 22:36:13 -0500

    Added Movie and User seeds

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+User.create(username: "michael", password: "test", email: "bfirestone2339@gmail.com")
+User.create(username: "marcus", password: "test", email: "bfirestone2339@gmail.com")
+User.create(username: "andrew", password: "test", email: "bfirestone2339@gmail.com")
+User.create(username: "jordan", password: "test", email: "bfirestone2339@gmail.com")
+
+Movie.create(title: "Parasite", director: "Bong Joon-Ho", release_date: "10/05/2019", genre: "Thriller")
+Movie.create(title: "Dunkirk", director: "Christopher Nolan", release_date: "07/21/2017", genre: "Action")
+Movie.create(title: "Brief Encounter", director: "David Lean", release_date: "08/24/1946", genre: "Drama")
+Movie.create(title: "The Lobster", director: "Yorgos Lanthimos", release_date: "10/16/2015", genre: "Drama")
+Movie.create(title: "Ex Machina", director: "Alex Garland", release_date: "04/10/2015", genre: "Sci-Fi")
+Movie.create(title: "Ponyo", director: "Hayao Miyazaki", release_date: "07/19/2008", genre: "Fantasy")
+
+
