c53a8492fc9bfb997ca10b9d90d04124718767d6
Brian Firestone <bfirestone2339@gmail.com>
2021-02-04 12:28:22 -0500

    Moved helper methods to application_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,14 +62,4 @@ class UsersController < ApplicationController
         end
     end
 
-    helpers do
-        def logged_in?
-            !!session[:user_id]
-        end
-
-        def current_user
-            User.find(session[:user_id])
-        end
-    end
-
 end
