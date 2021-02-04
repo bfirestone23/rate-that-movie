7f2d0fb8c4002c77173623fbec42e24cad096a9e
Brian Firestone <bfirestone2339@gmail.com>
2021-02-04 13:04:11 -0500

    Created /reviews POST action and /reviews/:id action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,7 +12,27 @@ class ReviewsController < ApplicationController
         end
     end
 
-    post '/reviews' do
+    get '/reviews/:id' do
         binding.pry
+        @review = Review.find_by_id(params[:id])
+        erb :'reviews/show'
+    end
+
+    post '/reviews' do
+        @user = User.find_by_id(session[:user_id])
+        if params[:existing_movie]
+            @movie = Movie.find_by_id(params[:existing_movie][:movie_id])
+            @review = Review.create(watch_date: params[:review][:watch_date], rating: params[:review][:rating], description: params[:review][:description])
+            @review.user = @user
+            @review.movie = @movie
+            @review.save
+        else
+            @movie = Movie.create(title: params[:new_movie][:title], genre: params[:new_movie][:genre], director: params[:new_movie][:director], release_date: params[:new_movie][:release_date])
+            @review = Review.create(watch_date: params[:review][:watch_date], rating: params[:review][:rating], description: params[:review][:description])
+            @review.user = @user
+            @review.movie = @movie
+            @review.save
+        end
+        redirect to "/reviews/#{@review.id}"
     end
 end
