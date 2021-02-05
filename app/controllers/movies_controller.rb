5cef5740c5b4cdc8b8af03de26d0aa33ac8f8e4b
Brian Firestone <bfirestone2339@gmail.com>
2021-02-05 13:45:11 -0500

    Added logged in verification to all actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,31 @@
 class MoviesController < ApplicationController
 
     get '/movies' do
-        erb :'movies/movies'
+        if logged_in?
+            erb :'movies/movies'
+        else
+            #Flash msg: You do not have access to this page, please log in or sign up
+            redirect to '/'
+        end
     end
 
     get '/movies/new' do
-        erb :'movies/new'
+        if logged_in?
+            erb :'movies/new'
+        else
+            #Flash msg: You do not have access to this page, please log in or sign up
+            redirect to '/'
+        end
     end
 
     get '/movies/:slug' do
-        @movie = Movie.find_by_slug(params[:slug])
-        erb :'movies/show'
+        if logged_in?
+            @movie = Movie.find_by_slug(params[:slug])
+            erb :'movies/show'
+        else
+            #Flash msg: You do not have access to this page, please log in or sign up
+            redirect to '/'
+        end
     end
 
     post '/movies' do
