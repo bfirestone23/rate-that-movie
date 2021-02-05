a4b39742f3593f38c44b8e47a2f9f3b552911f98
Brian Firestone <bfirestone2339@gmail.com>
2021-02-05 14:16:19 -0500

    Added '/movies/:slug/edit' and updated POST '/movies' action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -28,8 +28,29 @@ class MoviesController < ApplicationController
         end
     end
 
+    get '/movies/:slug/edit' do
+        if logged_in?
+            @movie = Movie.find_by_slug(params[:slug])
+            erb :'movies/edit'
+        else
+            #Flash msg: You do not have access to this page, please log in or sign up
+            redirect to '/'
+        end
+    end
+
     post '/movies' do
-        binding.pry
+        existing_movie = Movie.find_by(title: params[:title])
+        if params[:title] == "" || params[:director] == "" || params[:genre] == "" || params[:release_date] == ""
+            #Flash msg: Fields left blank - please try again.
+            redirect to '/movies/new'
+        elsif existing_movie
+            #Flash msg: Movie already exists in the database.
+            redirect to '/movies'
+        else
+            @movie = Movie.create(params)
+            #Flash msg: Movie successfully created
+            redirect to "/movies/#{@movie.slug}"
+        end
     end
 
 end
