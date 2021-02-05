91056b2b4e02589056b53f3a03240e000675d0fd
Brian Firestone <bfirestone2339@gmail.com>
2021-02-05 13:38:45 -0500

    Added '/movies/new' and '/movies/:slug' POST actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,9 +4,17 @@ class MoviesController < ApplicationController
         erb :'movies/movies'
     end
 
+    get '/movies/new' do
+        erb :'movies/new'
+    end
+
     get '/movies/:slug' do
         @movie = Movie.find_by_slug(params[:slug])
         erb :'movies/show'
+    end
+
+    post '/movies' do
+        binding.pry
     end
 
 end
