f3a788814ceb93bfd0acb5d4f32c9972e14dfbcf
Brian Firestone <bfirestone2339@gmail.com>
2021-02-05 14:59:41 -0500

    Added DELETE and PATCH actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,7 +21,12 @@ class MoviesController < ApplicationController
     get '/movies/:slug' do
         if logged_in?
             @movie = Movie.find_by_slug(params[:slug])
-            erb :'movies/show'
+            if @movie
+                erb :'movies/show'
+            else
+                #Flash msg: Movie does not exist
+                erb :'movies/movies'
+            end
         else
             #Flash msg: You do not have access to this page, please log in or sign up
             redirect to '/'
@@ -50,6 +55,44 @@ class MoviesController < ApplicationController
             @movie = Movie.create(params)
             #Flash msg: Movie successfully created
             redirect to "/movies/#{@movie.slug}"
+        end
+    end
+
+    patch '/movies/:slug' do
+        if params[:title] == "" || params[:director] == "" || params[:genre] == "" || params[:release_date] == ""
+            #Flash msg: Fields left blank - please try again.
+            @movie = Movie.find_by_slug(params[:slug])
+            redirect to "/movies/#{@movie.slug}/edit"
+        elsif Movie.find_by(title: params[:title])
+            #Flash msg: Movie already exists in the database.
+            @movie = Movie.find_by(title: params[:title])
+            redirect to "/movies/#{@movie.slug}"
+        else
+            @movie = Movie.find_by_slug(params[:slug])
+            @movie.title = params[:title]
+            @movie.director = params[:director]
+            @movie.genre = params[:genre]
+            @movie.release_date = params[:release_date]
+            @movie.save
+            #Flash msg: Movie successfully updated
+            redirect to "/movies/#{@movie.slug}"
+        end
+    end
+
+    delete '/movies/:slug' do 
+        if logged_in?
+            @movie = Movie.find_by_slug(params[:slug])
+            if @movie.reviews
+                #Flash msg: Movie has existing reviews, cannot delete
+                redirect to "/movies/#{@movie.slug}"
+            else
+                @movie.destroy
+                #Flash msg: movie successfully deleted
+                erb :'movies/movies'
+            end
+        else
+            #Flash msg: You do not have access to this action, please log in or sign up
+            redirect to '/'
         end
     end
 
