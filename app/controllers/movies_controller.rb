d3ac16340067485170550411ecd23b6f912c807e
Brian Firestone <bfirestone2339@gmail.com>
2021-02-05 13:25:22 -0500

    Added '/movies/:slug' action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,12 @@
 class MoviesController < ApplicationController
 
     get '/movies' do
-        erb :movies
+        erb :'movies/movies'
+    end
+
+    get '/movies/:slug' do
+        @movie = Movie.find_by_slug(params[:slug])
+        erb :'movies/show'
     end
 
 end
