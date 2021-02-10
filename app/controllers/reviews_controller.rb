d4adac6f2878dd3c7a5c38e26e25d4a524b057c4
Brian Firestone <bfirestone2339@gmail.com>
2021-02-10 13:11:19 -0500

    Added logged in and current user validation to /reviews/:id/edit, /reviews, /reviews/:id, and delete actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -27,57 +27,76 @@ class ReviewsController < ApplicationController
     get '/reviews/:id/edit' do
         @review = Review.find_by_id(params[:id])
         @user = @review.user
-        erb :'reviews/edit'
+
+        if logged_in? && @user == current_user
+            erb :'reviews/edit'
+            redirect to "/reviews/#{params[:id]}"
+        elsif !logged_in?
+            flash[:message] = "You do not have access to that page! Please log in or sign up below."
+            redirect to '/'
+        else 
+            flash[:message] = "You do not have permission to edit this review."
+            redirect to "/reviews/#{params[:id]}"
+        end
     end
 
     post '/reviews' do
         @user = User.find_by_id(session[:user_id])
-        if params[:existing_movie]
-            @movie = Movie.find_by_id(params[:existing_movie][:movie_id])
-            @review = Review.create(params[:review])
-            @review.user = @user
-            @review.movie = @movie
-            @review.save
-        elsif !params[:existing_movie] && !params[:new_movie][:title] == ""
-            flash[:message] = "Please select or add a movie!"
-            redirect to "/reviews/new"
-        else
-            @movie = Movie.create(params[:new_movie])
-            @review = Review.create(params[:review])
-            @review.user = @user
-            @review.movie = @movie
-            @review.save
+        if logged_in?
+            if params[:existing_movie]
+                @movie = Movie.find_by_id(params[:existing_movie][:movie_id])
+                @review = Review.create(params[:review])
+                @review.user = @user
+                @review.movie = @movie
+                @review.save
+            elsif !params[:existing_movie] && !params[:new_movie][:title] == ""
+                flash[:message] = "Please select or add a movie!"
+                redirect to "/reviews/new"
+            else
+                @movie = Movie.create(params[:new_movie])
+                @review = Review.create(params[:review])
+                @review.user = @user
+                @review.movie = @movie
+                @review.save
 
-            flash[:message] = "Review successfully created!"
+                flash[:message] = "Review successfully created!"
+            end
+            redirect to "/reviews/#{@review.id}"
+        else
+            flash[:message] = "You do not have access to that page! Please log in or sign up below."
+            redirect to '/'
         end
-        redirect to "/reviews/#{@review.id}"
     end
 
     patch '/reviews/:id' do
         @user = User.find_by_id(session[:user_id])
         @review = Review.find_by_id(params[:id])
-        if @user == current_user
-            if params[:existing_movie]
-                @movie = Movie.find_by_id(params[:existing_movie][:movie_id])
-                @review.movie = @movie
-                @review.update(params[:review])
-                @review.save
-            elsif !params[:existing_movie] && !params[:new_movie][:title] == ""
-                flash[:message] = "Please select or add a movie!"
-                redirect to "/reviews/#{@review.id}"
-            else
-                @movie = Movie.create(params[:new_movie])
-                @review.movie = @movie
-                @review.update(params[:review])
-                @review.save
+        if logged_in?
+            if @user == current_user
+                if params[:existing_movie]
+                    @movie = Movie.find_by_id(params[:existing_movie][:movie_id])
+                    @review.movie = @movie
+                    @review.update(params[:review])
+                    @review.save
+                elsif !params[:existing_movie] && !params[:new_movie][:title] == ""
+                    flash[:message] = "Please select or add a movie!"
+                    redirect to "/reviews/#{@review.id}"
+                else
+                    @movie = Movie.create(params[:new_movie])
+                    @review.movie = @movie
+                    @review.update(params[:review])
+                    @review.save
 
-                flash[:message] = "Review successfully created!"
+                    flash[:message] = "Review successfully created!"
+                end
+            else  
+                flash[:message] = "You do not have access to edit this review!"
+                redirect to "/reviews/#{@review.id}"
             end
-        else  
-            flash[:message] = "You do not have access to edit this review!"
-            redirect to "/reviews/#{@review.id}"
+        else
+            flash[:message] = "You do not have access to that page! Please log in or sign up below."
+            redirect to '/'
         end
-        redirect to "/reviews/#{@review.id}"
     end
 
     delete '/reviews/:id' do 
@@ -87,12 +106,15 @@ class ReviewsController < ApplicationController
                 @review.destroy
                 flash[:message] = "Review successfully deleted!"
                 redirect to "/welcome/#{@review.user.slug}"
+            elsif @review.user != current_user
+                flash[:message] = "You do not have access to delete this review!"
+                redirect to "/welcome/#{@review.user.slug}"
             else
                 flash[:message] = "You do not have access to delete this review!"
                 redirect to "/welcome/#{@review.user.slug}"
             end
         else
-            flash[:message] = "You are not logged in!"
+            flash[:message] = "You do not have access to that page! Please log in or sign up below."
             redirect to "/"
         end
     end
