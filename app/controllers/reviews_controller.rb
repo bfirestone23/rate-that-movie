65eec0880e6d72dc409f624a264a9bc5fc178683
Brian Firestone <bfirestone2339@gmail.com>
2021-06-04 14:09:42 -0700

    Replace all #logged_in? logic with #redirect_if_not_logged_in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,44 +1,36 @@
 class ReviewsController < ApplicationController
 
     get '/reviews' do
+        redirect_if_not_logged_in
+
         @request
         @reviews = Review.all.sort { |a, b| b.created_at <=> a.created_at }
-        if logged_in? 
-            erb :'reviews/reviews'
-        else
-            redirect to '/'
-        end
+        
+        erb :'reviews/reviews'
     end
 
     get '/reviews/new' do
-        if logged_in?
-            @movies = Movie.all.sort { |a, b| a.title <=> b.title }
-            erb :'reviews/new'
-        else
-            flash[:message] = "You do not have access to that page! Please log in or sign up below."
-            redirect to '/'
-        end
+        redirect_if_not_logged_in
+        
+        @movies = Movie.all.sort { |a, b| a.title <=> b.title }
+        erb :'reviews/new'
     end
 
     get '/reviews/:id' do
-        if logged_in?
-            @review = Review.find_by_id(params[:id])
-            erb :'reviews/show'
-        else
-            flash[:message] = "You do not have access to that page! Please log in or sign up below."
-            redirect to '/'
-        end
+        redirect_if_not_logged_in
+        
+        @review = Review.find_by_id(params[:id])
+        erb :'reviews/show'
     end
 
     get '/reviews/:id/edit' do
+        redirect_if_not_logged_in
+        
         @review = Review.find_by_id(params[:id])
         @user = @review.user
 
         if logged_in? && @user == current_user
             erb :'reviews/edit'
-        elsif !logged_in?
-            flash[:message] = "You do not have access to that page! Please log in or sign up below."
-            redirect to '/'
         else 
             flash[:message] = "You do not have permission to edit this review."
             redirect to "/reviews/#{params[:id]}"
@@ -46,19 +38,26 @@ class ReviewsController < ApplicationController
     end
 
     post '/reviews' do
+        redirect_if_not_logged_in
+
         @user = User.find_by_id(session[:user_id])
-        if logged_in?
-            if params[:existing_movie]
-                @movie = Movie.find_by_id(params[:existing_movie][:movie_id])
-                @review = Review.create(params[:review])
-                @review.user = @user
-                @review.movie = @movie
-                @review.save
-            elsif !params[:existing_movie] && !params[:new_movie][:title] == ""
-                flash[:message] = "Please select or add a movie!"
+        
+        if params[:existing_movie]
+            @movie = Movie.find_by_id(params[:existing_movie][:movie_id])
+            @review = Review.create(params[:review])
+            @review.user = @user
+            @review.movie = @movie
+            @review.save
+        elsif params[:new_movie][:title] == ""
+            flash[:message] = "Please add a movie!"
+            redirect to "/reviews/new"
+        else
+            @movie = Movie.create(params[:new_movie])
+
+            if params[:review][:rating] == ""
+                flash[:message] = "Please add a rating!"
                 redirect to "/reviews/new"
             else
-                @movie = Movie.create(params[:new_movie])
                 @review = Review.create(params[:review])
                 @review.user = @user
                 @review.movie = @movie
@@ -66,47 +65,39 @@ class ReviewsController < ApplicationController
 
                 flash[:message] = "Review successfully created!"
             end
-            redirect to "/reviews/#{@review.id}"
-        else
-            flash[:message] = "You do not have access to that page! Please log in or sign up below."
-            redirect to '/'
         end
+        redirect to "/reviews/#{@review.id}"
     end
 
     patch '/reviews/:id' do
+        redirect_if_not_logged_in
+
         @user = User.find_by_id(session[:user_id])
         @review = Review.find_by_id(params[:id])
-        if logged_in?
-            if @user == current_user
-                @review.update(params[:review])
-                @review.save
 
-                flash[:message] = "Review successfully updated!"
-                redirect to "/reviews/#{@review.id}"
-            else  
-                flash[:message] = "You do not have access to edit this review!"
-                redirect to "/reviews/#{@review.id}"
-            end
-        else
-            flash[:message] = "You do not have access to that page! Please log in or sign up below."
-            redirect to '/'
+        if @user == current_user
+            @review.update(params[:review])
+            @review.save
+
+            flash[:message] = "Review successfully updated!"
+            redirect to "/reviews/#{@review.id}"
+        else  
+            flash[:message] = "You do not have access to edit this review!"
+            redirect to "/reviews/#{@review.id}"
         end
     end
 
     delete '/reviews/:id' do 
-        if logged_in?
-            @review = Review.find_by_id(params[:id])
-            if @review && @review.user == current_user
-                @review.destroy
-                flash[:message] = "Review successfully deleted!"
-                redirect to "/welcome/#{@review.user.slug}"
-            else
-                flash[:message] = "You do not have access to delete this review!"
-                redirect to "/welcome/#{@review.user.slug}"
-            end
+        redirect_if_not_logged_in
+
+        @review = Review.find_by_id(params[:id])
+        if @review && @review.user == current_user
+            @review.destroy
+            flash[:message] = "Review successfully deleted!"
+            redirect to "/welcome/#{@review.user.slug}"
         else
-            flash[:message] = "You do not have access to that page! Please log in or sign up below."
-            redirect to "/"
+            flash[:message] = "You do not have access to delete this review!"
+            redirect to "/welcome/#{@review.user.slug}"
         end
     end
 end
