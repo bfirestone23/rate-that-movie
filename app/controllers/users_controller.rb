959c5028158dfaf041d4a771cf8f3836340f13d7
Brian Firestone <bfirestone2339@gmail.com>
2021-06-04 14:11:09 -0700

    Replace #logged_in? logic with #redirect_if_not_logged_in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,29 @@
 class UsersController < ApplicationController
 
     get '/users/:slug' do
-        if logged_in?
-            @user = User.find_by_slug(params[:slug])
-            @reviews = @user.reviews.sort { |a, b| b.created_at <=> a.created_at }
-            erb :'users/show'
-        else
-            flash[:message] = "You do not have access to that page! Please log in or sign up below."
-            redirect to '/'
-        end
+        redirect_if_not_logged_in
+
+        @user = User.find_by_slug(params[:slug])
+        @reviews = @user.reviews.sort { |a, b| b.created_at <=> a.created_at }
+
+        erb :'users/show'
     end
 
     get '/welcome/:slug' do 
         @request
-        if logged_in?
-            @user = User.find_by_slug(params[:slug])
-            @users = User.all
-            if @user == current_user
-                @reviews = @user.reviews.sort { |a, b| b.created_at <=> a.created_at }
-                erb :'users/welcome'
-            else
-                redirect to "/users/#{@user.slug}"
-            end
-        else
-            flash[:message] = "You do not have access to that page! Please log in or sign up below."
-            redirect to '/'
+        
+        redirect_if_not_logged_in
+
+        @user = User.find_by_slug(params[:slug])
+
+        if @user != current_user
+            redirect to "/users/#{@user.slug}"
         end
+
+        @users = User.all
+        @reviews = @user.reviews.sort { |a, b| b.created_at <=> a.created_at }
+
+        erb :'users/welcome'
     end
 
     get '/signup' do
@@ -33,9 +31,9 @@ class UsersController < ApplicationController
             @user = User.find_by_id(session[:user_id])
             flash[:message] = "You're already logged in!"
             redirect to "/welcome/#{@user.slug}"
-        else
-            erb :'users/signup'
         end
+
+        erb :'users/signup'
     end
 
     post '/signup' do
@@ -43,21 +41,23 @@ class UsersController < ApplicationController
             @user = User.find_by_id(session[:user_id])
             flash[:message] = "You're already logged in!"
             redirect to "/welcome/#{@user.slug}"
-        else
-            if params[:username] == "" || params[:password] == "" || params[:email] == ""
-                redirect to '/signup'
-            else
-                existing_user = User.find_by(username: params[:username])
-                if existing_user
-                    flash[:message] = "User already exists."
-                    redirect to '/signup'
-                else
-                    @user = User.create(params)
-                    session[:user_id] = @user.id
-                    erb :'users/welcome'
-                end
-            end
         end
+
+        if params[:username] == "" || params[:password] == "" || params[:email] == ""
+            flash[:message] = "All fields are required."
+            redirect to '/signup'
+        end
+
+        existing_user = User.find_by(username: params[:username])
+        if existing_user
+            flash[:message] = "User already exists."
+            redirect to '/signup'
+        end
+
+        @user = User.create(params)
+        session[:user_id] = @user.id
+        
+        redirect to "/welcome/#{@user.slug}"
     end
 
     get '/login' do
@@ -65,9 +65,9 @@ class UsersController < ApplicationController
             @user = User.find_by_id(session[:user_id])
             flash[:message] = "You're already logged in!"
             redirect to "/welcome/#{@user.slug}"
-        else
-            erb :'users/login'
         end
+
+        erb :'users/login'
     end
 
     post '/login' do
@@ -75,26 +75,25 @@ class UsersController < ApplicationController
             @user = User.find_by_id(session[:user_id])
             flash[:message] = "You're already logged in!"
             redirect to "/welcome/#{@user.slug}"
+        end
+
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to "/welcome/#{@user.slug}"
         else
-            @user = User.find_by(username: params[:username])
-            if @user && @user.authenticate(params[:password])
-                session[:user_id] = @user.id
-                redirect to "/welcome/#{@user.slug}"
-            else
-                flash[:message] = "Invalid username or password. Hint: they're case-sensitive!"
-                redirect to '/login'
-            end
+            flash[:message] = "Invalid username or password. Hint: they're case-sensitive!"
+            erb :'users/login'
         end
     end
 
     get '/logout' do 
-        if logged_in?
-            session.destroy
-            redirect to "/"
-        else
-            flash[:message] = "You are not logged in!"
-            redirect to '/'
-        end
+        redirect_if_not_logged_in
+
+        session.destroy
+        flash[:message] = "Goodbye!"
+        erb :index
     end
 
 end
